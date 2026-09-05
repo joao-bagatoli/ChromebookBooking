025406fccc7681d3f083bf4c9220b79ea1ab9ea4
João Bagatoli <bagatoli.dev@outlook.com>
2026-09-05 12:12:21 -0300

    feat: cancel booking

--- ChromebookBooking.Api/Interfaces/IBookingService.cs
+++ ChromebookBooking.Api/Interfaces/IBookingService.cs
@@ -1,8 +1,7 @@
-﻿using ChromebookBooking.Api.DTOs;
-
-namespace ChromebookBooking.Api.Interfaces;
+﻿namespace ChromebookBooking.Api.Interfaces;
 
 public interface IBookingService
 {
     Task BookAsync(DateOnly date, int classPeriodId, int teacherId, int sectionId, bool isPartial, int? chromebooksQuantity);
+    Task CancelBookingAsync(int bookingId, Guid authUserId);
 }
